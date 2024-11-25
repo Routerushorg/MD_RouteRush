6e9617a1c2b6547ed02df8dcb02ffe224b920ff2
Fraggerz777 <146559854+Fraggerz777@users.noreply.github.com>
2024-11-25 09:50:15 +0700

    Add Register and Login button functionalities

--- app/src/main/java/com/example/routerush/MainActivity.kt
+++ app/src/main/java/com/example/routerush/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.routerush
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.routerush.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
+        // Inisialisasi ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Event handler untuk tombol Login
+        binding.loginButton.setOnClickListener {
+            // Pindah ke LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Event handler untuk tombol Sign In
+        binding.signInButton.setOnClickListener {
+            // Pindah ke RegisterActivity
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
